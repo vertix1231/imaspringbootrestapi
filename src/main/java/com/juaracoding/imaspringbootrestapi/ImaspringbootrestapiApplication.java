02f1744344ae0bf147791d82f25ae34cd60be29f
Irsyad Muhammad Andre <irsyad.muhamad.andre@gmail.com>
2023-11-22 21:20:54 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/imaspringbootrestapi/ImaspringbootrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.imaspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaspringbootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaspringbootrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/imaspringbootrestapi/ImaspringbootrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.imaspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaspringbootrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
